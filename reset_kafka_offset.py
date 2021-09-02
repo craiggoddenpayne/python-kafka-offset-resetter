a9f4e4c88a6bf207703bde06d5caebe90cb07994
Craig <craig@goddenpayne.co.uk>
2021-09-02 12:44:54 +0100

    Add tool to reset a kafka offset

--- reset_kafka_offset.py
+++ reset_kafka_offset.py
@@ -1,15 +1,14 @@
-import getopt
 import sys
 from typing import List
-
 import confluent_kafka
 import yaml
-from confluent_kafka import TopicPartition, OFFSET_BEGINNING
+from confluent_kafka import TopicPartition, OFFSET_BEGINNING, OFFSET_END
 
 
 def main(argv):
     topics = ''
     config = ''
+    seek = ''
 
     try:
         index = 0
@@ -21,23 +20,38 @@ def main(argv):
             if arg == "-c":
                 config = argv[index]
 
+            if arg == "-s":
+                seek = argv[index]
+
     except Exception:
-        print('reset_kafka_offset.py -t=topics -c=config_file')
+        print('USAGE: python reset_kafka_offset.py -c config_file -t topics -s EARLIEST')
         sys.exit(2)
+
+    if topics == '' or config == '' or seek == '':
+        print('Some parameters were not supplied')
+        print('USAGE: python reset_kafka_offset.py -c config_file -t topics -s EARLIEST')
+        sys.exit(3)
 
     print('Topics are ', topics)
     print('Config file is located ', config)
-    print('This script will seek a kafka consumer to a specific offset of EARLIEST')
+    print(f'This script will seek a kafka consumer to a specific offset of {seek}')
     result = input('Press `y` to continue')
     if result != 'y':
+        print('Cancelled')
         sys.exit(1)
 
-    reset_stream_to_0(topics, config)
+    seek_to(topics, config, seek)
 
 
-def reset_stream_to_0(topics: List[str], config_path: str):
+def seek_to(topics: List[str], config_path: str, seek: str):
+
+    if seek == 'LATEST':
+        seek = OFFSET_END
+    if seek == 'EARLIEST':
+        seek = OFFSET_BEGINNING
+    print(f'Seek has been transformed to `{seek}`')
+
     # load the config file
-    config_properties = {}
     with open(config_path, 'r') as stream:
         config_properties = yaml.safe_load(stream)
 
@@ -46,11 +60,12 @@ def reset_stream_to_0(topics: List[str], config_path: str):
     for topic in topics:
         kafka_topic = consumer.list_topics().topics[topic]
         for partition in kafka_topic.partitions:
-            kafka_partition = TopicPartition(topic=topic, partition=partition, offset=OFFSET_BEGINNING)
+            kafka_partition = TopicPartition(topic=topic, partition=partition, offset=seek)
+            print(f'Current Position: {consumer.position([kafka_partition])}')
             consumer.assign([kafka_partition])
             consumer.seek(kafka_partition)
             consumer.commit()
-            print(f'Reset offset to earliest for {topic}:{partition}')
+            print(f'New Position: {consumer.position([kafka_partition])}')
 
 
 if __name__ == "__main__":
